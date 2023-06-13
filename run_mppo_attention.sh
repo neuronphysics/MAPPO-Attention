3bc97eaa32a292ff0dfe70ad78ef82592939e7a1
Zahra Sheikhbahaee <memole@gra-login1.graham.sharcnet>
2023-06-13 15:55:33 -0400

    using wandb

--- run_mppo_attention.sh
+++ run_mppo_attention.sh
@@ -17,19 +17,20 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/memole/.mujoco/mujoco210/bin
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/nvidia
 conda create -n marl python==3.8.10
 conda activate marl
-
+export WANDB_API_KEY ='a2a1bab96ebbc3869c65e3632485e02fcae9cc42'
+export WANDB_ENTITY ='zsheikhb'
 pip install torch==2.0.1+cu118 torchvision torchaudio -f https://download.pytorch.org/whl/torch_stable.html
 pip install -r requirements.txt
 # install this package first
-pip install seaborn
-pip install absl-py 
-pip install gym 
-pip install wandb 
-pip install tensorboardX 
-pip install torch-ac
-pip install imageio 
-pip install pyglet 
-pip install PIL
+pip install --no-index seaborn
+pip install --no-index absl-py 
+pip install --no-index gym 
+pip install --no-index wandb
+pip install --no-index tensorboardX 
+pip install --no-index torch-ac
+pip install --no-index imageio 
+pip install --no-index pyglet 
+pip install --no-index PIL
 # install on-policy package
 
 pip install -e .
@@ -38,13 +39,12 @@ pip install -e .
 #based on https://github.com/oxwhirl/pymarl/tree/master
 bash install_sc2.sh
 #Hanabi
-pip install cffi
-cd /home/memole/Montreal/on-policy/onpolicy/envs/hanabi
+pip install --no-index cffi
+cd /home/memole/Montreal/MPPO-ATTENTIOAN/onpolicy/envs/hanabi
 mkdir build & cd build
 cmake ..
 make -j
 # install this package first
-pip install seaborn
 #football environment
 apt-get update
 apt-get install libsdl2-gfx-dev libsdl2-ttf-dev
@@ -57,6 +57,6 @@ mkdir -p football/third_party/gfootball_engine/lib
 wget https://storage.googleapis.com/gfootball/prebuilt_gameplayfootball_v2.8.so -O football/third_party/gfootball_engine/lib/prebuilt_gameplayfootball.so
 cd football && GFOOTBALL_USE_PREBUILT_SO=1 python3 -m pip install .
 
-cd /home/memole/Montreal/on-policy/onpolicy/scripts/train_mpe_scripts
+cd /home/memole/Montreal/MPPO-ATTENTIOAN/onpolicy/scripts/train_mpe_scripts
 chmod +x train_mpe_comm.sh
 bash train_mpe_comm.sh
