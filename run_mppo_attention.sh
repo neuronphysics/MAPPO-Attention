bd72489fe4ffa899a04784047cc12bd8b8b2ec1e
Zahra Sheikhbahaee <memole@gra-login3.graham.sharcnet>
2023-06-22 13:26:37 -0400

    update the installation process

--- run_mppo_attention.sh
+++ run_mppo_attention.sh
@@ -1,61 +1,57 @@
 #!/bin/bash
-#
+#SBATCH --job-name=MPPO
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:v100:2
-#SBATCH --tasks-per-node=2
-#SBATCH --cpus-per-task=5
-#SBATCH --mem=30G
-#SBATCH --time=01-13:59
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=16
+#SBATCH --gres=gpu:v100l:2
+#SBATCH --mem=95000M   
+#SBATCH --time=1-20:00:00
 #SBATCH --account=def-gdumas85
-#SBATCH --output=/home/memole/Montreal/MPPO-ATTENTIOAN/logs/MPPO-attention-seed-1_%N-%j.out
-#SBATCH --error=/home/memole/Montreal/MPPO-ATTENTIOAN/logs/MPPO-attention-seed-1_%N-%j.err
-module load StdEnv/2020
-module load python/3.10
-module load scipy-stack
-virtualenv --no-download $HOME/marl_envs
-source $HOME/marl_envs/bin/activate
+#SBATCH --output=~/projects/def-gdumas85/memole/MPPO-ATTENTIOAN/logs/MPPO-attention-seed-1_%N-%j.out
+#SBATCH --error=~/projects/def-gdumas85/memole/MPPO-ATTENTIOAN/logs/MPPO-attention-seed-1_%N-%j.err
+#SBATCH --mail-user=sheikhbahaee@gmail.com              # notification for job conditions
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+module load gcc python/3.10 opencv
+#virtualenv --no-download --clear ~/Montreal/marl_envs
+
+git clone https://neuronphysics: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@github.com/neuronphysics/MPPO-ATTENTIOAN.git
+virtualenv --no-download --clear $SLURM_TMPDIR
+source $SLURM_TMPDIR/bin/activate
+cd MPPO-ATTENTIOAN/
+PATH=`pwd`
 pip install --no-index --upgrade pip
 pip install --no-index torch torchvision torchtext torchaudio
-pip install -r requirements.txt
 
-pip install --no-index seaborn
-pip install --no-index absl-py 
-pip install --no-index gym 
-pip install --no-index wandb
-pip install --no-index tensorboardX 
-pip install --no-index torch-ac
-pip install --no-index imageio 
-pip install --no-index pyglet 
-pip install --no-index PIL
-wandb login a2a1bab96ebbc3869c65e3632485e02fcae9cc42
+pip install -r requirements.txt
+python -m pip install git+https://github.com/mpi4py/mpi4py
+pip install --no-cache-dir mpyq
+pip install --no-cache-dir mujoco-py
+pip install --no-index --no-cache-dir torch-ac
+
+pip install 'git+https://github.com/IntelPython/mkl_fft.git'
+pip install 'git+https://github.com/IntelPython/mkl_random.git'
+wandb login $API_KEY
 # install this package first
 
 # install on-policy package
-
 pip install -e .
 
 #install starcraft
-#based on https://github.com/oxwhirl/pymarl/tree/master
-bash install_sc2.sh
+export SC2PATH='/home/memole/Montreal/StarCraftII'
 #Hanabi
-pip install --no-index cffi
-cd /home/memole/Montreal/MPPO-ATTENTIOAN/onpolicy/envs/hanabi
-mkdir build & cd build
-cmake ..
-make -j
-# install this package first
-#football environment
-apt-get update -y
-apt-get install -y libsdl2-gfx-dev libsdl2-ttf-dev
 
-# 2.8 and 2.9 binary is the same, so we use 2.8 .so file
-#https://github.com/google-research/football/blob/master/README.md
-git clone -b v2.8 https://github.com/google-research/football.git
-mkdir -p football/third_party/gfootball_engine/lib
 
-wget https://storage.googleapis.com/gfootball/prebuilt_gameplayfootball_v2.8.so -O football/third_party/gfootball_engine/lib/prebuilt_gameplayfootball.so
-cd football && GFOOTBALL_USE_PREBUILT_SO=1 pip3 install .
+cd $PATH/onpolicy/envs/hanabi/
+cmake -B _build -S .
+cmake --build _build
+python -c 'import pyhanabi'
+##install this package first
+##football environment
+#python3 -m pip install --upgrade pip setuptools psutil wheel
+#python3 -m pip install gfootball
 
-cd /home/memole/Montreal/MPPO-ATTENTIOAN/onpolicy/scripts/train_mpe_scripts
+cd $PATH/onpolicy/scripts/train_mpe_scripts
 chmod +x train_mpe_comm.sh
 bash train_mpe_comm.sh
