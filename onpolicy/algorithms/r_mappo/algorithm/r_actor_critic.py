Fixed kl loss not propogating
added it to wanddb

onpolicy/algorithms/r_mappo/algorithm/r_actor_critic.py
@@ -12,6 +12,7 @@ from onpolicy.algorithms.utils.popart import PopArt
 from onpolicy.utils.util import get_shape_from_obs_space
 from absl import logging
 from onpolicy.algorithms.utils.cnn import Decoder, Decoder_base
+from Causal_MRBL.models.losses import kl_loss
 class R_Actor(nn.Module):
     """
     Actor network class for MAPPO. Outputs actions given observations.
@@ -192,7 +193,7 @@ class R_Actor(nn.Module):
                 eps = torch.randn_like(std)
                 z = std * eps + mu
 
-                kl_div = torch.nn.functional.kl_div(actor_features, z)
+                kl_div = kl_loss(mu,logvar)
 
                 actor_features = z
             
@@ -214,7 +215,7 @@ class R_Actor(nn.Module):
                 eps = torch.randn_like(std)
                 z = std * eps + mu
 
-                kl_div = torch.nn.functional.kl_div(actor_features,z)
+                kl_div =  kl_loss(mu,logvar)
 
                 actor_features = z
 

onpolicy/algorithms/r_mappo/r_mappo.py
@@ -142,7 +142,8 @@ class R_MAPPO():
         if self.use_recon is not None:
             recon_loss = image_loss(torch.from_numpy(recon_batch), torch.from_numpy(obs_batch))
             if self.use_kl is not None:
-                recon_loss += kl_batch
+
+                recon_loss += kl_batch[0]
 
         if update_actor and self.use_recon:
 
@@ -173,8 +174,11 @@ class R_MAPPO():
 
         self.policy.critic_optimizer.step()
 
-        if self.use_recon:
-         return value_loss, critic_grad_norm, policy_loss, dist_entropy, actor_grad_norm, imp_weights, recon_loss
+        if self.use_recon and self.use_kl:
+         return value_loss, critic_grad_norm, policy_loss, dist_entropy, actor_grad_norm, imp_weights, recon_loss, kl_batch[0]
+
+        elif self.use_recon:
+          return value_loss, critic_grad_norm, policy_loss, dist_entropy, actor_grad_norm, imp_weights, recon_loss,
 
         else:
             return value_loss, critic_grad_norm, policy_loss, dist_entropy, actor_grad_norm, imp_weights,

onpolicy/utils/separated_buffer.py
@@ -58,7 +58,7 @@ class SeparatedReplayBuffer(object):
         self.bad_masks = np.ones_like(self.masks)
         self.active_masks = np.ones_like(self.masks)
         self.reconstructions = np.zeros((self.episode_length + 1, self.n_rollout_threads, *obs_shape), dtype=np.float32)
-        self.kl_divs = None
+        self.kl_divs = 0
         self.step = 0
         
     def update_factor(self, factor):
@@ -79,7 +79,7 @@ class SeparatedReplayBuffer(object):
         if reconstructions is not None:
             self.reconstructions[self.step+1] = reconstructions.copy()
         if kl_divs is not None:
-            self.kl_divs[self.step] = kl_divs.copy()
+            self.kl_divs = kl_divs
 
         if bad_masks is not None:
             self.bad_masks[self.step + 1] = bad_masks.copy()
@@ -105,7 +105,7 @@ class SeparatedReplayBuffer(object):
         if reconstructions is not None:
             self.reconstructions[self.step] = reconstructions.copy()
         if kl_divs is not None:
-            self.kl_divs[self.step] = kl_divs.copy()
+            self.kl_divs= kl_divs
 
         if bad_masks is not None:
             self.bad_masks[self.step + 1] = bad_masks.copy()
@@ -127,8 +127,7 @@ class SeparatedReplayBuffer(object):
 
         if self.reconstructions is not None:
             self.reconstructions[0] = self.reconstructions[-1].copy()
-        if self.kl_divs is not None:
-            self.kl_divs[0] = self.kl_divs[-1].copy()
+
 
         if self.available_actions is not None:
             self.available_actions[0] = self.available_actions[-1].copy()
@@ -343,7 +342,7 @@ class SeparatedReplayBuffer(object):
 
         actions = _cast(self.actions)
         if self.kl_divs is not None:
-            kl_divs = _cast(self.kl_divs)
+            kl_divs = self.kl_divs
         action_log_probs = _cast(self.action_log_probs)
         advantages = _cast(advantages)
         value_preds = _cast(self.value_preds[:-1])
@@ -374,7 +373,8 @@ class SeparatedReplayBuffer(object):
             adv_targ = []
             reconstructions_batch = []
             kl_batch = []
-
+            if self.kl_divs is not None:
+             kl_batch.append(kl_divs)
             for index in indices:
                 ind = index * data_chunk_length
                 # size [T+1 N M Dim]-->[T N Dim]-->[N T Dim]-->[T*N,Dim]-->[L,Dim]
@@ -382,8 +382,8 @@ class SeparatedReplayBuffer(object):
                 obs_batch.append(obs[ind:ind+data_chunk_length])
                 if self.reconstructions is not None:
                     reconstructions_batch.append(reconstructions[ind:ind+data_chunk_length])
-                if self.kl_divs is not None:
-                    kl_batch.append(kl_divs[ind:ind+data_chunk_length])
+
+
                 actions_batch.append(actions[ind:ind+data_chunk_length])
                 if self.available_actions is not None:
                     available_actions_batch.append(available_actions[ind:ind+data_chunk_length])
@@ -405,7 +405,6 @@ class SeparatedReplayBuffer(object):
             obs_batch = np.stack(obs_batch)
             if self.kl_divs is not None:
                 kl_batch = np.stack(kl_batch)
-                kl_batch = _flatten(L, N, kl_batch)
 
             actions_batch = np.stack(actions_batch)
             if self.available_actions is not None: