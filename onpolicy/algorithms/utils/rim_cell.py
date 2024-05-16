ee00e24a4f84cdc81932bedb48f9e9a263aa4a6a
sstevec <su19chang@gmail.com>
2024-05-15 22:52:53 -0400

    put null input back for rim

--- onpolicy/algorithms/utils/rim_cell.py
+++ onpolicy/algorithms/utils/rim_cell.py
@@ -258,8 +258,8 @@ class RIMCell(nn.Module):
 
         if self.use_input_att:
             # Compute input attention
-            # null_input = torch.zeros(batch_size, 1, input_size).float().to(self.device)
-            # x = torch.cat((x, null_input), dim=1)
+            null_input = torch.zeros(batch_size, 1, input_size).float().to(self.device)
+            x = torch.cat((x, null_input), dim=1)
             inputs, mask = self.input_attention_mask(x, hs)
             mask = mask.unsqueeze(-1)
         else:
