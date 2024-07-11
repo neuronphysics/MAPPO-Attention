0ba5483dcb73d8f72f3508b0bc1b5ee9a4fa537a
Zahra Sheikhbahaee <sheikhbahaee@gmail.com>
2024-07-11 17:11:12 -0400

    modify pretrain command line

--- run_mappo_pretrain_slot_att_QSA.sh
+++ run_mappo_pretrain_slot_att_QSA.sh
@@ -44,8 +44,8 @@ srun python3 train_meltingpot.py --use_valuenorm False --use_popart True --env_n
      --critic_lr 0.00002 --n_rollout_threads 1 --max_grad_norm 0.01 --use_wandb True --user_name "zsheikhb" --wandb_name "zsheikhb" \
      --share_policy False --use_centralized_V False --use_attention True --entropy_coef 0.004 --attention_module "RIM" --rim_num_units 6 \
      --rim_topk 4 --hidden_size 300 --num_env_steps 4000000 --log_interval 1 --episode_length 1000 --downsample True --img_scale_factor 1 \
-     --world_img_scale_factor 8 --pretrain_slot_att False --slot_train_ep 150 --slot_pretrain_batch_size 40 \
+     --world_img_scale_factor 8 --pretrain_slot_att False --slot_train_ep 150 --slot_pretrain_batch_size 100 \
      --slot_att_work_path "/home/mila/z/zahra.sheikhbahaee/Projects/meltingpot/slot_att_qsa/onpolicy/scripts/results/slot_att/" \
-     --slot_att_load_model True --use_slot_att True --use_pos_encoding False --use_input_att False --use_com_att True \
-     --use_x_reshape True --slot_att_crop_repeat 9 --slot_log_fre 1 --collect_data False --collect_agent False \
-     --collect_world True --collect_data_ep_num 20 --no_train False --crop_size 88 --lr_main 0.00005 --lr_dvae 0.00015
+     --slot_att_load_model False --use_slot_att False --use_pos_encoding False --use_input_att False --use_com_att True --use_x_reshape True \
+     --slot_att_crop_repeat 9 --slot_log_fre 1 --collect_data True --collect_agent False --collect_world True --collect_data_ep_num 20 \
+     --no_train True --crop_size 88
