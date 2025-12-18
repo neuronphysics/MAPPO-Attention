fixing image normalization for slot attention

onpolicy/algorithms/r_mappo/algorithm/r_actor_critic.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 
 from onpolicy.algorithms.utils.lstm import LSTMLayer
-from onpolicy.algorithms.utils.util import print_trainable_parameters, init, check, ObsDataset, selectively_unfreeze_layers
+from onpolicy.algorithms.utils.util import print_trainable_parameters, init, check, ObsDataset, selectively_unfreeze_layers, _normalize_slot_obs
 from onpolicy.algorithms.utils.cnn import CNNBase, Encoder
 from onpolicy.algorithms.utils.modularity import SCOFF
 from onpolicy.algorithms.utils.mlp import MLPBase
@@ -181,6 +181,8 @@ class R_Actor(nn.Module):
             # slot att model takes (batch, 3, H, W) and returns a dict
             torch.cuda.empty_cache()
             batch, _, _, _ = obs.shape
+            # normalize the obs to [0, 1]
+            obs= _normalize_slot_obs(obs)
 
             # your slot attention or other GPU-intensive tasks
             slot_outputs = self.slot_attn(obs.permute(0, 3, 1, 2), tau=self.tau, sigma=self.sigma, is_Train= True,
@@ -259,7 +261,8 @@ class R_Actor(nn.Module):
             torch.cuda.empty_cache()  # Free up GPU memory
             with torch.no_grad():
                 batch, _, _, _ = obs.shape
-            
+                # normalize the obs to [0, 1]
+                obs= _normalize_slot_obs(obs)
                 features = torch.cat([
                        self.slot_attn(
                            obs_minibatch.permute(0, 3, 1, 2),

onpolicy/algorithms/utils/QSA/data_loader.py
@@ -5,7 +5,7 @@ import random
 from PIL import Image, ImageFile
 from torchvision import transforms
 from torch.utils.data import Dataset
-
+from onpolicy.algorithms.utils.QSA.utils import rescale 
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 
@@ -38,7 +38,7 @@ class GlobDataset(Dataset):
                 image_paths = sorted(glob.glob(os.path.join(dir, img_glob)))
                 for path in image_paths:
                     data_tmp = torch.load(path).permute(0, 3, 1, 2)
-                    self.episodes.extend(data_tmp / 255.0)
+                    self.episodes.extend(data_tmp.float() / 255.0)
 
         if world_root is not None and world_root != "":
             train_split = int(len(self.world_total_dirs) * train_split_percent)
@@ -62,7 +62,7 @@ class GlobDataset(Dataset):
                     for t in data_tmp:
                         for i in range(self.crop_repeat):
                             cropped_img = self.random_crop_img(t)
-                            self.episodes.append(cropped_img / 255.0)
+                            self.episodes.append(cropped_img.float() / 255.0) #images in [0, 1]
 
         self.episodes = torch.stack(self.episodes, dim=0)
 

onpolicy/algorithms/utils/QSA/slot_attn.py
@@ -37,7 +37,7 @@ class SlotAttention(nn.Module):
         )
 
         self.drop_path = DropPath(drop_path) if drop_path > 0 else nn.Identity()
-        print(self.truncate)
+        print(f"SlotAttention truncate method: {self.truncate}")
         assert self.truncate in ['bi-level', 'fixed-point', 'none']
 
     def forward(self, features, slots_init, num_iter=3):